fc6dd3866f60945a4b0ba9b0448944596cac99ca
Sumit Kumar <sumitkuo@in.ibm.com>
2025-12-18 20:31:03 +0530

    Bumping to 7.0.1.0  (#20)
    * Fixing podman network
    
    Signed-off-by: Sumit Kumar <sumitkuo@in.ibm.com>
    
    * Bumping to 7.0.1.0
    
    Signed-off-by: Sumit Kumar <sumitkuo@in.ibm.com>
    
    ---------
    
    Signed-off-by: Sumit Kumar <sumitkuo@in.ibm.com>

--- classes/rclmgr_yml.py
+++ classes/rclmgr_yml.py
@@ -72,7 +72,7 @@ CONFIG_rclmgr_YML = {
     'UTILITY_HOSTNAME': 'utilityBareMetal',
     'CAMPUS_INTERFACE': 'campus',
     'CAMPUS_INTERFACE_IP': '192.168.100.10',
-    'IMAGE_VERSION': '7.0.0.0'
+    'IMAGE_VERSION': '7.0.1.0'
 }
 
 
@@ -396,10 +396,10 @@ class rclmgr_yml(object):
                     "Going to ask the user for a Image Version"
                 )
                 IMAGE_VERSION_user = input(
-                    "Enter the image version (default: 7.0.0.0): "
+                    "Enter the image version (default: 7.0.1.0): "
                 )
                 if IMAGE_VERSION_user == "":
-                    IMAGE_VERSION_user = "7.0.0.0"
+                    IMAGE_VERSION_user = "7.0.1.0"
                     break
                 elif IMAGE_VERSION_user == "6.2.3.0" or \
                         IMAGE_VERSION_user == "6.2.3.1" or \
@@ -407,7 +407,7 @@ class rclmgr_yml(object):
                         IMAGE_VERSION_user == "7.0.0.0":
                     break
                 else:
-                    print("Image name should be 6.2.3.0 or 6.2.3.1 or 6.2.3.2 or 7.0.0.0\n")
+                    print("Image name should be 6.2.3.0 or 6.2.3.1 or 6.2.3.2 or 7.0.0.0 or 7.0.1.0\n")
             return IMAGE_VERSION_user
         except KeyboardInterrupt:
             print("")
